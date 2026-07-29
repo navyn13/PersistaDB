8da50b254ee93ff30ba8d6c795b7a73f3628a4ff
Naveen <navyn13102003@gmail.com>
2026-07-29 17:38:10 +0530

    Enhance message handling in server to validate command arguments for set, get, and delete operations, returning appropriate error messages for invalid commands.

--- internal/server/handler.go
+++ internal/server/handler.go
@@ -10,6 +10,10 @@ func (s *Server) handleMessage(msg Message) error {
 
 	switch parts[0] {
 	case "set":
+		if len(parts) != 3 {
+			msg.peer.Send([]byte("INVALID SET COMMAND\r\n"))
+			return nil
+		}
 		err := s.wal.Write(msg.data)
 		if err != nil {
 			msg.peer.Send([]byte("ERROR WRITING TO WAL\r\n"))
@@ -19,7 +23,24 @@ func (s *Server) handleMessage(msg Message) error {
 		return nil
 
 	case "get":
+		if len(parts) != 2 {
+			msg.peer.Send([]byte("INVALID GET COMMAND\r\n"))
+			return nil
+		}
 		slog.Info("Getting value", "key", parts[1])
+		return nil
+
+	case "delete":
+		if len(parts) != 2 {
+			msg.peer.Send([]byte("INVALID DELETE COMMAND\r\n"))
+			return nil
+		}
+		err := s.wal.Write(msg.data)
+		if err != nil {
+			msg.peer.Send([]byte("ERROR DELETING TO WAL\r\n"))
+			return err
+		}
+		msg.peer.Send([]byte("OK\r\n"))
 		return nil
 	default:
 		msg.peer.Send([]byte("INVALID COMMAND\r\n"))
